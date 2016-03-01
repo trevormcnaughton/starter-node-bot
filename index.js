85c89838d5d6d4e2996b2d28a9d18f2e11285f45
trevormcnaughton <trevor.mcnaughton@gmail.com>
2016-02-29 18:48:37 -0700

    new bot stuff

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var Botkit = require('botkit')
+var config = require('./config.json');
+var clearbitToken = config.tokens.clearbit;
+var slackToken = config.tokens.slack;// || process.env.SLACK_TOKEN
+//var clearbit = require('clearbit')(clearbitToken);
 
 // Expect a SLACK_TOKEN environment variable
-var slackToken = process.env.SLACK_TOKEN
 if (!slackToken) {
   console.error('SLACK_TOKEN is required!')
   process.exit(1)
@@ -14,7 +17,8 @@ var bot = controller.spawn({
 
 bot.startRTM(function (err, bot, payload) {
   if (err) {
-    throw new Error('Could not connect to Slack')
+    console.log(err);
+    throw new Error('Could not connect to Slack');
   }
 })
 
@@ -66,3 +70,37 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
 })
+
+
+// Clearbit lookup
+
+controller.hears('lookup (.*)', 'direct_message,direct_mention', function(bot, message){
+  var matches = message.text.match(/lookup (.*)/i);
+  var email = matches[1].split('|')[1].slice(0, -1);
+
+  clearbit.Enrichment.find({email: email})
+    .then(function(response) {
+      bot.reply(message, handlePersonProfileFormatting(response));
+    })
+    .catch(clearbit.Enrichment.QueuedError, function (err) {
+      console.log(err);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+});
+
+function handlePersonProfileFormatting(response) {
+  const person = response.person;
+  const company = response.company;
+  const details = {
+    github: function() { return person.github && person.github.handle ? 'Github: https://github.com/' + person.github.handle : '' },
+    twitter: function() { return person.twitter && person.twitter.handle? 'Twitter: https://twitter.com/' + person.twitter.handle : '' },
+    linkedin: function() { return person.linkedin && person.linkedin.handle? 'LinkedIn: https://linkedin.com/' + person.linkedin.handle : '' },
+    angellist: function() { return person.angellist && person.angellist.handle? 'AngelList: https://angel.co/' + person.angellist.handle : '' }
+  }
+
+  var message = person.name.fullName + '\n' + details.github() + '\n' + details.twitter() + '\n' + details.linkedin() + '\n' + details.angellist();
+
+  return message;
+}
